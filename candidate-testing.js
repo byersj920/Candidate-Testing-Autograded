941f5a637894fd9112a8f9cb4a084e18f4082820
Jonathan <byersj920@gmail.com>
2023-09-25 11:31:17 -0500

    Project Part 2 Submission

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,9 +10,19 @@ let correctAnswer = 'Sally Ride';
 let canidateAnswer ='';
 
 //TODO: Variables for Part 2
-let questions;
-let canidateAnswers;
-let correctAnswers;
+let questions =[
+  'Who was the first American woman in space? ', 
+  'True or false: 5 kilometer == 5000 meters? ', 
+  '(5 + 3)/2 * 10 = ? ', 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  "What is the minimum crew size for the ISS? "];
+let canidateAnswers = [];
+let correctAnswers=[
+  'Sally Ride',
+  'true',
+  '40',
+  'Trajectory',
+  "3"];
 
 
 function askForName() {
@@ -22,17 +32,17 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-canidateAnswer = input.question(`${question}`);
+//canidateAnswer = input.question(`${question}`);
+for (i = 0; i < questions.length; i++){
+  canidateAnswers.push(input.question(`${questions[i]}`));
+}
 }
 
 function gradeQuiz() {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-if (canidateAnswer.toLowerCase().trim() === 'sally ride'){
-  console.log('Correct!');
-} else {
-  console.log(`Incorrect. The correct answer was "Sally Ride."`);
+for (i = 0; i < canidateAnswers.length; i++){
+  console.log(`For question ${i+1}, you answered '${canidateAnswers[i]}'. The correct answer was ${correctAnswers[i]}.`);
 }
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -44,7 +54,7 @@ if (canidateAnswer.toLowerCase().trim() === 'sally ride'){
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log(`Hi, ${canidateName}! Nice to meet'cha. Let's get started with the quiz. There will be five questions in this quiz. Good luck!`);
+   console.log(`Hi, ${canidateName}! Nice to meet'cha. Let's get started with the quiz. There will be five questions. Good luck!`);
   askQuestion();
   gradeQuiz();
 }
